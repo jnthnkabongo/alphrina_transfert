052e3f709687c388838af05cd3d2f4d9191456f4
jonathan <jnthnkabongo@gmail.com>
2024-03-16 22:50:36 +0100

    avancement

--- app/Http/Requests/saveDette.php
+++ app/Http/Requests/saveDette.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class creentials extends FormRequest
+class saveDette extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,8 +22,7 @@ class creentials extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => 'required|email',
-            'password' => 'required|min:3'
+            
         ];
     }
 }
